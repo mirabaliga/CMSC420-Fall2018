0044e837eef1f0a77550fcfa3a1155720837bccf
Jason Filippou <jason.filippou@gmail.com>
2018-08-30 22:02:45 -0400

    Wrote version 1 of docs.

--- src/projects/graph/AdjacencyListGraph.java
+++ src/projects/graph/AdjacencyListGraph.java
@@ -3,8 +3,22 @@ package projects.graph;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * <p>{@link AdjacencyListGraph} is a {@link Graph} implemented as an adjacency list, i.e a one-dimensional array of linked lists,
+ * where A(i) is a linked list containing the neighbors of node i and the corresponding edges' weights. <b>The neighbors of a given node are defined as the nodes it  points to</b> (if any). </p>
+ *
+ * <p>Other implementations besides linked lists are possible (e.g BSTs over the weight of the edge), yet for this project we
+ * will keep it simple and stick to that basic implementation. One of its advantages is that, because the lists do not need
+ * to be sorted in any way, the insertion of a new edge is a O(1) operation (find the list corresponding to the source node in O(1)
+ * and add the new list node up front.</p>
+ *
+ * @author --- YOUR NAME HERE! ---
+ *
+ * @see Graph
+ * @see AdjacencyMatrixGraph
+ * @see SparseAdjacencyMatrixGraph
+ */
 public class AdjacencyListGraph implements Graph {
-
 
     @Override
     public void addNode() {
@@ -54,10 +68,20 @@ public class AdjacencyListGraph implements Graph {
 
     /* Methods specific to this class follow. */
 
+    /**
+     * <p>Transforms <tt>this</tt> into an instance of {@link AdjacencyMatrixGraph}. This is an O(E) operation. </p>
+     *
+     * @return An instance of {@link AdjacencyMatrixGraph}.
+     */
     public AdjacencyMatrixGraph toAdjacencyMatrixGraph(){
         return null;
     }
 
+    /**
+     * <p>Transforms <tt>this</tt> into an instance of {@link AdjacencyMatrixGraph}. This is an O(E) operation. </p>
+     *
+     * @return An instance of {@link AdjacencyMatrixGraph}.
+     */
     public SparseAdjacencyMatrixGraph toSparseAdjacencyMatrixGraph(){
         return null;
     }
--- src/projects/graph/AdjacencyMatrixGraph.java
+++ src/projects/graph/AdjacencyMatrixGraph.java
@@ -3,6 +3,27 @@ package projects.graph;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * <p>{@link AdjacencyMatrixGraph} is a {@link Graph} implemented as an <b>adjacency matrix</b>. An adjacency matrix
+ * is a V x V matrix where M(i, j) is the weight of the edge from i to j. If there is no edge between i and j,
+ * the weight should be zero. </p>
+ *
+ * <p>Adjacency matrices answer {@link #isConnected(int, int)} in O(1) time. Insertion and deletion of edges, as well
+ * as retrieval of the weight of a given edge  are all O(1) operations as well. Retrieval of all neighbors of a given node
+ * happens in O(V) time. </p>
+ *
+ * <p>The main drawbacks of adjacency matrices are: </p>
+ *  <ol>
+ *      <li>They occupy O(V^2) <b>contiguous</b> memory space, which for sparse graphs can be a significant memory footprint. </li>
+ *      <li>addNode() runs in O(V^2) time, since new array storage needs to be allocated for the extra row and column,
+ *      and the old data need be copied to the new array. </li>
+ *  </ol>
+ *
+ * @author --- YOUR NAME HERE! ---
+ * @see Graph
+ * @see SparseAdjacencyMatrixGraph
+ * @see AdjacencyListGraph
+ */
 public class AdjacencyMatrixGraph implements Graph {
 
     @Override
@@ -53,10 +74,24 @@ public class AdjacencyMatrixGraph implements Graph {
 
     /* Methods specific to this class follow. */
 
+    /**
+     * <p>Returns a sparsified representation of the adjacency matrix, i.e a linked list of its non-null elements (non-zero,
+     * in this case) and their coordinates. The matrix should be scanned in <b>row-major order</b> to populate
+     * the elements of the list (<b>and we test for proper element insertion order!</b>). </p>
+     *
+     * @return A {@link SparseAdjacencyMatrixGraph} instance.
+     */
     public SparseAdjacencyMatrixGraph toSparseAdjacencyMatrixGraph(){
         return null;
     }
 
+    /**
+     * <p>Returns a representation of the {@link Graph} as an {@link AdjacencyListGraph}. Remember that an {@link AdjacencyListGraph}
+     * is implemented as an array of linked lists, where A(i) is a linked list containing the neighbors of node i.  Remember that an
+     * {@link AdjacencyListGraph} is implemented as an array of linked lists, where A(i) is a linked list containing the neighbors of node i.
+     *
+     * @return  An {@link AdjacencyListGraph} instance.
+     */
     public AdjacencyListGraph toAdjacencyListGraph(){
         return null;
     }
