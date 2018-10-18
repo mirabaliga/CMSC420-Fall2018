c34316486f718e40bd051b5f232333d16906bb6b
Jason Filippou <jason.filippou@gmail.com>
2018-10-18 15:00:46 -0400

    Made sure to mask top bit of hash function.

--- src/projects/phonebook/hashes/LinearProbingHashTable.java
+++ src/projects/phonebook/hashes/LinearProbingHashTable.java
@@ -27,7 +27,7 @@ public class LinearProbingHashTable implements HashTable{
     private KVPair[] table;
 
     private int hash(String key){
-        return key.hashCode() % table.length;
+        return (key.hashCode() & 0xffffff) % table.length;
     }
 
     /*  YOU SHOULD ALSO IMPLEMENT THE FOLLOWING 2 METHODS ACCORDING TO THE SPECS
